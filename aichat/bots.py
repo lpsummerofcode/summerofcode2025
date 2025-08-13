adf6ceaee744955d8c5a2f4ff87a2c95c5e3afdf
Mason Leo <masonleo10008@gmail.com>
2025-08-13 09:58:38 -0400

    Object Model

--- aichat/bots.py
+++ aichat/bots.py
@@ -1,10 +1,11 @@
 
 class Bot:
-    def __init__(self, name: str, description: str):
+    def __init__(self, name: str, description: str, emoji: str):
         self.name = name
         self.description = description
+        self.emoji = emoji
         self.messages = []
     
     def append_message(self, message: str):
         self.messages.append(message)
-
+        
